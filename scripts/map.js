3c66680f780b7190fbf8fe884513567ce04dbffb
chevonnelise <elise.chevonne@gmail.com>
2023-01-08 23:56:05 +0000

    add filterlayer

--- scripts/map.js
+++ scripts/map.js
@@ -14,17 +14,18 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 const resultLayer = L.layerGroup();
 resultLayer.addTo(map);
 
+
 var taxiIcon = L.icon({
   iconUrl: "../images/taxi.svg",
-  iconSize: [50,50]
+  iconSize: [20,20]
 });
 
 var mrtIcon = L.icon({
-  iconUrl: "../images/mrt.svg",
+  iconUrl: "../images/mrt.svg"
 })
 
 var programmeIcon = L.icon({
-  iconUrl: '../images/programme-heart.svg',
+  iconUrl: '../images/programme-heart.svg'
 })
 
 loadData();
@@ -71,6 +72,17 @@ document.querySelector("#search-btn").addEventListener("click", async function()
 });
 
 
+document.querySelector("#search-btn").addEventListener('DOMContentLoaded', async function(){
+  const programmesResponse = await axios.get("geoJson/programmes.geojson");
+  const programmesCoordinates = programmesResponse.data.features[0].geometry.coordinates;
+  let programmesMarkerClusterLayer = L.markerClusterGroup();
+  for (let i = 0; i < programmesCoordinates.length; i++) {
+    let pos = programmesCoordinates[i];
+    L.marker([pos[1],pos[0]],{icon:programmeIcon}).bindPopup(`Service/Programme(s) at Lat: ${pos[1]}, Lon:${pos[0]}`).addTo(programmesMarkerClusterLayer);
+  }
+  programmesMarkerClusterLayer.addTo(map);
+})
+
 window.addEventListener('DOMContentLoaded', async () => {
   // get taxi data and plot on map
   const taxiResponse = await axios.get("../geoJson/taxi-availability.geojson");
@@ -79,7 +91,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   let taxiMarkerClusterLayer = L.markerClusterGroup();
   for (let i = 0; i < taxiCoordinates.length; i++) {
     let pos = taxiCoordinates[i];
-    L.marker([pos[1],pos[0]],{icon:taxiIcon}).bindPopup(`Taxi at Lat: ${pos[1]}, Lon:${pos[0]}`).addTo(taxiMarkerClusterLayer);
+    L.marker([pos[1],pos[0]],{icon:taxiIcon}).bindPopup(`Service/Programme(s) at Lat: ${pos[1]}, Lon:${pos[0]}`).addTo(taxiMarkerClusterLayer);
   }
   taxiMarkerClusterLayer.addTo(map);
 
@@ -89,14 +101,14 @@ window.addEventListener('DOMContentLoaded', async () => {
     onEachFeature: function (feature, layer) {
       if (feature.type == "station") {
         layer.bindPopup(`
-          <div style="min-width:300px">
-            <h1>${feature.properties.name}</h1>
+          <div style="min-width:20px">
+            <h4>${feature.properties.name}</h4>
           </div>
           `)
       } else {
         layer.bindPopup(`
           <div>
-            <h1>${feature.properties.name}</h1>       
+            <h4>${feature.properties.name}</h4>       
           </div>
           `)
       }
@@ -105,98 +117,50 @@ window.addEventListener('DOMContentLoaded', async () => {
   mrtLayer.addTo(map);
 })
 
-//search for programmes
-document.querySelector('#search-btn-modal').addEventListener('click', async () => {
+let programmesLayer = L.layerGroup();
+
+document.querySelector("#filtered-toggle").addEventListener("change", function(){
+  if(this.checked){
+    programmesLayer.addTo(map);
+  } else {
+    map.removeLayer(programmesLayer);
+  }
+})
+
+// search for programmes
+document.querySelector('#filtered-toggle').addEventListener('click', async () => {
   // clear previous markers
   resultLayer.clearLayers()
+  
   // get user's input
   let userInput = document.querySelector('#search-terms-programmes').value
+  
   // load programmes data
   const programmesDataResponse = await axios.get("../geoJson/programmes.geojson");
+
   // array of objects
   let programmes = programmesDataResponse.data.features
+
   // loop through all programmes and get the result related to the user's search
   for (let i = 0; i < programmes.length; i++) {
-    const service = programmes[i].properties['Service/Programme(s)'];
     // check there is service/programmes
-    if (service && service.includes(userInput)) {
+    if (programmes[i].properties['Service/Programme(s)']) {
+      // check that it includes user's input
+      if (!(programmes[i].properties['Service/Programme(s)'].search(userInput) == -1)) {
         // plot the marker
-        const lat = parseFloat(programmes[i].geometry.coordinates[0]).toFixed(4);
-        const lng = parseFloat(programmes[i].geometry.coordinates[1]).toFixed(4);
+        const lat = parseFloat(parseFloat(programmes[i].geometry.coordinates[0]).toFixed(4));
+        const lng = parseFloat(parseFloat(programmes[i].geometry.coordinates[1]).toFixed(4));
         const marker = L.marker([lng, lat])
         // marker.addTo(resultLayer)
-        marker.bindPopup(service) 
+        marker.bindPopup(programmes[i].properties['Service/Programme(s)'])
+        
         marker.addTo(programmesLayer);
       }
     }
+
     programmesLayer.addTo(map);
+    
+  }
+
 })
-
-
-// let programmesMarkerClusterLayer = L.layerGroup();
-// let taxiMarkerClusterLayer = L.layerGroup();
-// let mrtLayer = L.layerGroup();
-
-
-// function addCircleMarkersToLayers(
-//   layer,
-//   data,
-//   color
-// ) {
-//   for (let i =0; i <data.length; i++){
-//     L.circle(data[i].coordinates, {
-//       color: color,
-//       fillColor: color,
-//       fillOpacity:0.5,
-//       radius: 250
-//     }).addTo(layer).bindPopup(data[i].name);
-//   }
-// }
-
-// addCircleMarkersToLayers(programmesMarkerClusterLayer,programmesResponse,"green")
-// addCircleMarkersToLayers(taxiMarkerClusterLayer,taxiResponse,"blue")
-// addCircleMarkersToLayers(mrtLayer,mrtResponse,"red")
-
-const baseLayers = {};
-
-const  overlays = {
-      "Taxi": taxiMarkerClusterLayer,
-      "Mrt": mrtLayer
-}
-L.control.layers(baseLayers, overlays).addTo(map)
-
-
-// Create filter layer
-// Create layer groups for each category
-const programmesMarkerClusterLayer = L.layerGroup();
-const taxiMarkerClusterLayer = L.layerGroup();
-const mrtLayer = L.layerGroup();
-
-// Add markers to each layer group
-for (let i = 0; i < data.length; i++);{
-  const category = data[i].category;
-  const lat = data[i].category;
-  const lng = data[i].longitude;
-  const marker = L.marker([lat.lng]);
-  if (category === 'programmes') {
-    marker.addTo(programmesMarkerClusterLayer);
-  } else if (category === 'taxi');
-    marker.addTo(taxiMarkerClusterLayer);
-  // } else if (category === 'mrt');
-  //   marker.addTo(mrtLayer);
-
-if (selectedFilter === 'programmes') {
-  map.addLayer(programmesMarkerClusterLayer);
-  map.removeLayer(taxiMarkerClusterLayer);
-  map.removeLayer(mrtLayer);
-} else if (selectedFilter === 'taxi') {
-  map.addLayer(taxiMarkerClusterLayer);
-  map.removeLayer(programmesMarkerClusterLayer);
-  map.removeLayer(mrtLayer);
-} else if (selectedFilter === 'mrt') {
-  map.addLayer(mrtLayer);
-  map.removeLayer(taxiMarkerClusterLayer);
-  map.removeLayer(programmesMarkerClusterLayer);
-}
-}
 
